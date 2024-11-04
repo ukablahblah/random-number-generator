016d4d03fec27822b8564ca2dbf558418bfd20b5
ukablahblah <utkarshbhagatkumar@gmail.com>
2024-11-03 21:59:26 -0800

    "Add random number generator script"

---
+++ random_number_generator.py
@@ -0,0 +1,8 @@
+import random
+
+def generate_random_number():
+    number = random.randint(1, 100)
+    print(f"Generated Random Number: {number}")
+
+if __name__ == "__main__":
+    generate_random_number()
